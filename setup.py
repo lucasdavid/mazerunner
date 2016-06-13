ec3fb6d2c13158cb4e5ed9d24e5aaeb0392536cc
lucasdavid <AdmGitLucas107>
2016-06-13 01:39:12 -0300

    Add config files

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='mazerunner',
+    description='A behavior implemented for NAO Robot on V-REP environment.',
+    long_description=open('README.md').read(),
+    version='0.1',
+    packages=['mazerunner'],
+    scripts=[],
+    author='Lucas David',
+    author_email='lucasolivdavid@gmail.com',
+
+    url='https://github.com/lucasdavid/mazerunner',
+    download_url='https://github.com/lucasdavid/mazerunner/archive/master.zip',
+    install_requires=[],
+    tests_require=open('requirements-dev.txt').readlines(),
+)
