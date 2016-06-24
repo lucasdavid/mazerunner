47541571cea0366004d24a1ced99e8f870d139f4
agnaldoae <agnaldo.campinas@gmail.com>
2016-06-24 18:44:13 -0300

    .

--- mazerunner/reinforcementLearning/QLearning.py
+++ mazerunner/reinforcementLearning/QLearning.py
@@ -1,27 +1,34 @@
 """Reinforcement Learning functions
 
-Authors: Karina e Agnaldo
+Authors: Karina  --- 
+		 Agnaldo --- agnaldo.esmael@ic.unicamp.br
 
 """
 import numpy as np
 import random
 import math
 
-class QLearning(goalPosition, percept):
-#goalPosition eh um vetor de duas posicoes:x,y (tipo float)
-#percept eh um vetor de 5 posicoes:x_atual_NAO, y_atual_NAO, sonar_frente, sonar_atras, sonar_ombroD, sonarOmbroE (tipo float)
+# A classe QLearning -- determina as acoes do robo NAO
+# Actions = {0,1,2,3}, onde 0=ir para frente, 1=ir para tras, 2=vire aa Dir, 3=vire aa Esq.
+# percept eh um vetor float de 5 posicoes com as seguintes distancias em metros: 
+#   percept[0]= distancia(NAO, objetivo)
+#   percept[1]= distancia captada pelo sonar_frente
+#   percept[2]= distancia captada pelo sonar_atras
+#   percept[3]= distancia captada pelo sonar_ombroDir
+#   percept[4]= distancia captada pelo sonar_ombroEsq
+
+class QLearning(object):
 	
-	def __init__(self):
-		self.gp = goalPosition
+	def __init__(self, percept):
 		self.numStates = 1024
 		self.numAction = 4
 		self.alpha = 0.2
 		self.gamma = 0.75
-		self.atualA = 0		# inicia com um valor qq -- vai ser atualiza na primeira chamada de getAction
-		self.E = 0.2		# 20 cm -- Usado para determinar se o NAO mudou significativamente de posicao
-		self.atualS_binary = self.stateDiscretization(percept)
-		self.atualS = int(self.atualS_binary, 2)  # binay -> int
-		self.oldDist = getDist(percept[0],percept[1])
+		self.action = None
+		self.E = 0.2			# 20 cm -- Usado para determinar se o NAO mudou significativamente de posicao
+		self.state_binary = self.stateDiscretization(percept)
+		self.state_int = int(self.state_binary, 2)  # binay -> int
+		self.distNAOtoObj = percept[0] # recebe a distancia q o NAO estah do objetivo
 		self.Q = self.initQ()
 
 	def initQ(self):
@@ -36,27 +43,27 @@ class QLearning(goalPosition, percept):
 
 	#Lucas requisita uma nova acao 
 	def getAction(self):
-		self.atualA = np.argmax(Q[atualS])
-		return self.atualA
+		self.action = np.argmax(Q[self.state_int])
+		return self.action
 
 	# Atualiza a tabela Q e estados apos realizar a acao instruida pelo getAction
 	def setState(self, percept):
-		r = self.reward(self.atualS_binary,self.oldA)
+		r = self.reward(self.state_binary, self.action)
 		newS_binary = self.stateDiscretization(percept)
-		newS = int(newS_binary, 2)    #binary --> int
-		Q[self.atualS][self.atualA] = (1-self.alpha)*(Q[self.atualS][self.atualA]) + self.alpha*(r + self.gamma*(np.max(Q[newS])))
+		newS_int = int(newS_binary, 2)    #binary --> int
+		Q[self.state_int][self.action] = (1-self.alpha)*(Q[self.state_int][self.action]) + self.alpha*(r + self.gamma*(np.max(Q[newS_int])))
 		# atualizacoes
-		self.atualS = newS
-		self.atualS_binary = newS_binary
-		self.oldDist = getDist(percept[0],percept[1])
+		self.state_int = newS_int
+		self.state_binary = newS_binary
+		self.distNAOtoObj = percept[0]
 		return 
 
 	#funcao para discretizar o estado correspondente aa 'percept' --- gera uma string de bits 0s e 1s
 	def stateDiscretization(self, p):
 		s = ''
-		deltaDist = self.oldDist - self.getDist(p[0],p[1])
-		s = s + self.auxStateDisc(deltaDist, (-1)*self.E, self.E) # s=xx -> aproximou/manteve/recuou
-		s = s + self.auxStateDisc(p[2], 0.2, 0.5)   # s=xxyy  -> acrescimo: sonar da frente
+		deltaDist = self.distNAOtoObj - p[0]
+		s = s + self.auxStateDisc(deltaDist, (-1)*self.E, self.E) # s=xx -> 00-aproximou, 01-manteve, 10-recuou 
+		s = s + self.auxStateDisc(p[2], 0.2, 0.5)   # s=xxyy  -> acrescimo: sonar da frente --> 00-longe, 01-perto, 10-colisao
 		s = s + self.auxStateDisc(p[3], 0.2, 0.5)   # s=xxyyzz  -> acrescimo: sonar de tras
 		s = s + self.auxStateDisc(p[4], 0.1, 0.3)   # s=xxyyzzdd  -> acrescimo: sonar direita
 		s = s + self.auxStateDisc(p[5], 0.1, 0.3)   # s=xxyyzzddee  -> acrescimo: sonar esquerda
@@ -71,8 +78,8 @@ class QLearning(goalPosition, percept):
 		return b
 
 	# calcula a distancia do NAO ate o objetivo
-	def getDist(self, x, y):
-		return math.sqrt((self.gp[0]-x)**2 + (self.gp[1]-y)**2)
+	#def getDist(self, x, y):
+	#	return math.sqrt((self.gp[0]-x)**2 + (self.gp[1]-y)**2)
 
 	#funcao de recompensa
 	def reward(self, s, a):
